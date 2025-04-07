470b210f54b45b492f8a5977d668a5574fd2579a
Otto <ottoparnanen1@gmail.com>
2025-04-07 15:17:32 +0300

    Remove TODO comments and update CSS for improved layout and styling

--- src/components/SingleView.jsx
+++ src/components/SingleView.jsx
@@ -3,12 +3,6 @@ import React from 'react';
 const SingleView = (props) => {
   const {item, setSelectedItem} = props;
   return (
-    // TODO: Add JSX for displaying a mediafile here
-    // - use e.g. a <dialog> element for creating a modal
-    // - use item prop to render the media item details
-    // - use img tag for displaying images
-    // - use video tag for displaying videos
-
     <>
       {item && (
         <dialog open>
