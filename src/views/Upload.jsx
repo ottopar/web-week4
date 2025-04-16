be615c8a938a20378ccc0146bf21d0dfbc23db9b
Otto <ottoparnanen1@gmail.com>
2025-04-16 11:24:27 +0300

    Implement file upload functionality and update base path in Vite config

--- src/views/Upload.jsx
+++ src/views/Upload.jsx
@@ -1,8 +1,89 @@
-import React from 'react';
+import React, {useState} from 'react';
 import PropTypes from 'prop-types';
+import {useNavigate} from 'react-router';
+import {useFile, useMedia} from '../hooks/apiHooks';
+import useForm from '../hooks/formHooks';
 
 const Upload = () => {
-  return <div>Upload</div>;
+  const [file, setFile] = useState(null);
+  const {postFile} = useFile();
+  const {postMedia} = useMedia();
+  const navigate = useNavigate();
+
+  const doUpload = async () => {
+    try {
+      const token = window.localStorage.getItem('token');
+      const fileResult = await postFile(file, token);
+      console.log('fileResult', fileResult);
+      const mediaResult = await postMedia(fileResult.data, inputs, token);
+      console.log('mediaResult', mediaResult);
+
+      navigate('/');
+    } catch (error) {
+      console.log('error', error);
+    }
+  };
+
+  const {inputs, handleInputChange, handleSubmit} = useForm(doUpload);
+
+  const handleFileChange = (evt) => {
+    if (evt.target.files) {
+      console.log(evt.target.files[0]);
+      // Todo: set the file to state
+      setFile(evt.target.files[0]);
+    }
+  };
+
+  return (
+    <>
+      <h1>Upload</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="title">Title</label>
+          <input
+            name="title"
+            type="text"
+            id="title"
+            onChange={handleInputChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="description">Description</label>
+          <textarea
+            name="description"
+            rows={5}
+            id="description"
+            onChange={handleInputChange}
+          ></textarea>
+        </div>
+        <div>
+          <label htmlFor="file">File</label>
+          <input
+            name="file"
+            type="file"
+            id="file"
+            accept="image/*, video/*"
+            onChange={handleFileChange}
+          />
+        </div>
+        <img
+          src={
+            file
+              ? URL.createObjectURL(file)
+              : 'https://via.placeholder.com/200?text=Choose+image'
+          }
+          alt="preview"
+          width="200"
+        />
+        <button
+          type="submit"
+          disabled={file && inputs.title.length > 3 ? false : true}
+        >
+          Upload
+        </button>
+      </form>
+    </>
+  );
 };
 
 Upload.propTypes = {};
